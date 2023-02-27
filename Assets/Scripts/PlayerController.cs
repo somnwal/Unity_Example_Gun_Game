ec71fdfecf8922e91618ec3537c489587b31765d
솜놜 <somnwal.kor@gmail.com>
2023-02-27 21:23:28 +0900

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    private Vector2 moveInput;
+
+    public Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+
+        //transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed, moveInput.y * Time.deltaTime * moveSpeed, 0f);
+
+        rigidBody.velocity = moveInput * moveSpeed;
+
+        // 마우스 위치 얻어오기
+        Vector3 mousePosition = Input.mousePosition;
+        
+        Vector3 
+
+    }
+}
